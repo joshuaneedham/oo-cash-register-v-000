c5ffa82b66214f1ce767902a7e638e01f98bcc48
Joshua Needham <joshuabneedham@gmail.com>
2017-11-09 03:30:01 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,6 +8,12 @@ class CashRegister
   end
 
   def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+      self.last_transaction = amount * quantity
+    end
   end
 
 
